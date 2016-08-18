f0df0781a01efdd33b8a3e73ec3ce3cc193d441f
wnamen <williamjosephnamen@gmail.com>
2016-08-18 16:51:08 -0700

    updated keypress to calculate time

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -2,4 +2,25 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  var begin;
+  var end;
+  var total;
+
+  $(window).on("keypress", function timer(event) {
+    console.log(event);
+
+    if ((event.keyCode == 32) && (begin)) {
+      end = Date.now();
+      total = (end - begin) / 1000;
+      $('#total-time').text(total + " seconds");
+
+      begin = 0;
+      end = 0;
+
+    } else if (event.keyCode == 32) {
+      begin = Date.now();
+    }
+
+  })
+
 })
