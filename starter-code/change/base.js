9c58819889f92f11ad78de5c91a8fbb5af7ce3c0
wnamen <williamjosephnamen@gmail.com>
2016-08-18 15:44:07 -0700

    updated to change to actively calculate

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,14 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
+  $("input.add").on("change", function handleChange(event){
+
+    var left = parseInt($('#left').val()) || 0;
+    var right = parseInt($('#right').val()) || 0;
+    var total = left + right;
+
+    $('#total').val(total);
+
+  })
+
 })
