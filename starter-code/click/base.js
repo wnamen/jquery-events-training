2394ac4ef23cb9244f0a62ec9ff7981b34155a81
wnamen <williamjosephnamen@gmail.com>
2016-08-18 16:11:45 -0700

    updated clicks to message the user

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -2,4 +2,9 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  $('span').on("click", function handleClick(event) {
+    var time = Date.now();
+    var message = "You Clicked: " + $(this).text() + " at " + time;
+    $('ul').append("<li>" + message + "</li>");
+  })
 })
